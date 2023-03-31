e2c9f2f2bb0ec65e7660309ab754f8c48a92529e
HlibPanchenko <glebpanchenk7@gmail.com>
2023-03-31 17:19:12 +0300

    TS + RT, useAppDispatch , Enum, actions, state

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router} from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { store } from "./redux/store.js";
+import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
